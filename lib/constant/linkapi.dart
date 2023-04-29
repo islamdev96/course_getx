0b7010cb7409d040735024b3a0ace89c949d7caf
islamdev96 <islamsayedglab@gmail.com>
2023-04-29 11:33:19 +0200

    S

--- lib/constant/linkapi.dart
+++ lib/constant/linkapi.dart
@@ -1,10 +1,10 @@
-const String linkServerName = "https:///coursephp" ;
-const String linkImageRoot = "https://waelabohamza.com/coursephp/upload" ;
-// Auth 
-const String linkSignUp = "$linkServerName/auth/signup.php" ;  
-const String linkLogin = "$linkServerName/auth/login.php" ; 
-// Note 
-const String linkViewNotes    = "$linkServerName/notes/view.php" ;  
-const String linkAddNotes     = "$linkServerName/notes/add.php" ;  
-const String linkEditNotes    = "$linkServerName/notes/edit.php" ;  
-const String linkDeleteNotes  = "$linkServerName/notes/delete.php" ;  
+const String linkServerName = "http://10.0.2.2/coursephp";
+const String linkImageRoot = "$linkServerName/upload";
+// Auth
+const String linkSignUp = "$linkServerName/auth/signup.php";
+const String linkLogin = "$linkServerName/auth/login.php";
+// Note
+const String linkViewNotes = "$linkServerName/notes/view.php";
+const String linkAddNotes = "$linkServerName/notes/add.php";
+const String linkEditNotes = "$linkServerName/notes/edit.php";
+const String linkDeleteNotes = "$linkServerName/notes/delete.php";
